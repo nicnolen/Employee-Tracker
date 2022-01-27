c23d61b0e0815d1d64b788e3907425eacc455d00
Nicholas Nolen <nicnolen@ymail.com>
2022-01-27 15:40:14 -0600

    Added in employee table

--- db/schema.sql
+++ db/schema.sql
@@ -11,5 +11,13 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
 );
