6775d64866446ce1230c25027ac2c2da9691d1b4
Nicholas Nolen <nicnolen@ymail.com>
2022-01-28 11:41:41 -0600

    got the command-line prompts to work

--- app.js
+++ app.js
@@ -5,32 +5,60 @@ const inquirer = require('inquirer');
 
 // Start server after database connection
 db.connect(err => {
-    if (err) throw err;
-    console.info('Database connected.');
-})
+  if (err) throw err;
+  console.info('Database connected.');
+  promptUser();
+});
 
-// Inquirer prompt for the first action 
+// Inquirer prompt for the first action
 const promptUser = () => {
-    inquirer.prompt ([
-        {
-            type: 'list',
-            name: 'choices',
-            message: 'What would you like to do?',
-            choices: ['View All Departments',
-                      'View all Roles',
-                      'View All Employees',
-                      'Add a Department',
-                      'Add a Role',
-                      'Add an Employee',
-                      'Update an Employee Role',
-                      'Update Employee Manager',
-                      'Update Employees by Manager',
-                      'View Employees by Department',
-                      'Delete a Department',
-                      'Delete a Role',
-                      'Delete an Employee',
-                      'View Department Budgets',
-                      'Quit']
-        }
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'choices',
+        message: 'What would you like to do?',
+        choices: [
+          'View All Departments',
+          'View all Roles',
+          'View All Employees',
+          'Add a Department',
+          'Add a Role',
+          'Add an Employee',
+          'Update an Employee Role',
+          'Update Employee Manager',
+          'Update Employees by Manager',
+          'View Employees by Department',
+          'Delete a Department',
+          'Delete a Role',
+          'Delete an Employee',
+          'View Department Budgets',
+          'Quit',
+        ],
+      },
     ])
-}
+    // If a certain answer is chosen, run that function
+    .then(answers => {
+      const { choices } = answers;
+
+      if (choices === 'View All Departments') {
+        showDepartments();
+      }
+
+      if (choices === 'No Action') {
+        connection.end();
+      }
+    });
+};
+
+// Function to show all departments
+showDepartments = () => {
+  console.info('Showing all departments');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
