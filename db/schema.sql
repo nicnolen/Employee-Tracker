f03ca9894d1e23432cac693997e5cd159bfc4a1c
Nicholas Nolen <nicnolen@ymail.com>
2022-01-28 08:22:10 -0600

    populated the department table

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,26 @@
 /* SETS UP WHAT OUR TABLE WILL LOOK LIKE */
-/* Drops the department table if it exists */
+-- Drops the department table if it exists
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    /* Ensures that id cant be inserted in the role table if there isnt an id in the departments table */
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,9 @@
 /* POPULATES DATABASE */
+-- Populate Department table
+INSERT INTO department (name)
+VALUES 
+('Marketing'),
+('Finance'),
+('Operations Management'),
+('Human Resource'),
+('IT');
