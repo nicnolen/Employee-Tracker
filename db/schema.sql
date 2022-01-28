4ecbc8493f79e6203949746a2a5ded51db140154
Nicholas Nolen <nicnolen@ymail.com>
2022-01-28 09:04:09 -0600

    populated the role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 /* SETS UP WHAT OUR TABLE WILL LOOK LIKE */
 /* Drops the department table if it exists */
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,7 +13,7 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER,
     /* Ensures that id cant be inserted in the role table if there isnt an id in the departments table */
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,35 @@
 /* POPULATES DATABASE */
+-- Populate department table
+INSERT INTO department (name)
+VALUES 
+('Marketing'),
+('Finance'),
+('Operations Management'),
+('Human Resource'),
+('IT');
+
+-- Populate role table
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Chief Marketing Officer', 247250, 1),
+('Market Research Analyst', 65810, 1),
+('Public Relations Specialist', 52039, 1),
+('Financial Branch Manager', 68035, 2),
+('Financial Analyst', 88814, 2),
+('Financial Examiner', 62938, 2),
+('Operations Manager', 107670, 3),
+('Operations Analyst', 61457, 3),
+('Operations Officer', 79931, 3),
+('HR Director', 160483, 4),
+('Recruiter', 74750, 4),
+('HR Coordinator', 61000, 4),
+('IT Operations Manager', 106548, 5),
+('Software Engineer', 108249, 5),
+('Full Stack Web Developer', 106132, 5);
+
+
+
+
+
+
+
